fa05b0e39bc71c80960aeee5917796aadc15108d
Michael Ea <michael.dewerdt@gmail.com>
2022-06-21 13:42:26 +0200

    ex00 finished.

--- CPP00/ex00/megaphone.cpp
+++ CPP00/ex00/megaphone.cpp
@@ -1,17 +1,6 @@
 #include <iostream>
 
-int main(int argc; char **argv)
-{
-	char *tmp;
-
-	tmp = malloc(sizeof(char) * ft_strlen(str));
-	while (str[i])
-	{
-		tmp[i] = str[i] + 42;
-	}
-	ft_putstr(tmp);
-	return (0);
-}
+using namespace std;
 
 int		main(int argc, char **argv)
 {
@@ -23,7 +12,7 @@ int		main(int argc, char **argv)
 	while (--argc)
 	{
 		argv++;
-		for (int i = 0; i < strlen(*argv); i++)
+		for (size_t i = 0; i < strlen(*argv); i++)
 			(*argv)[i] = std::toupper((*argv)[i]);
 		std::cout << *argv << " ";
 	}
